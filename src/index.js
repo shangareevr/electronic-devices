c652fff06e4d05fc802f53500dd4d7dd9fd7956b
unknown <sh.rusell@gmail.com>
2023-12-19 23:19:00 +0500

    fix:config

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './index.html';
-import './style.scss';
-require("@fancyapps/fancybox");
-import './js/slider.js';
-import './js/mask-number.js';
-import './js/map.js';
-import './img/map.svg';
-import './js/burger-menu.js';
-import './js/border-animatiom.js';
--- src/js/mask-number.js
+++
@@ -1,9 +0,0 @@
-var $ = jQuery.noConflict();
-
-document.addEventListener('DOMContentLoaded',()=>{
-  jQuery(function($){
-    $(".call__number").mask("+7 (999)-999-99-99");
-    $(".questions__number").mask("+7 (999)-999-99-99");
-    $(".modal__number").mask("+7 (999)-999-99-99");
-  });
-})
