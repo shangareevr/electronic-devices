dfe3ab9eefc43dc71226b930b7b44bee40a2c949
unknown <sh.rusell@gmail.com>
2023-12-19 23:34:16 +0500

    fix:overlay

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './index.html';
 import './style.scss';
-require("@fancyapps/fancybox");
 import './js/slider.js';
 import './js/mask-number.js';
 import './js/map.js';
 import './img/map.svg';
 import './js/burger-menu.js';
 import './js/border-animatiom.js';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  Fancybox.bind("[data-fancybox]", {
+  });
+})
